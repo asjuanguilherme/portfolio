da2546b97f7710c3438b54275cceca3f38bab6b5
Juan Guilherme <asjuanguilherme@gmail.com>
2022-04-30 09:43:45 -0300

    refactor: :construction: Clean initial structure
    Delete default next template files and move pages to src folder

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <>
+    <Head>
+      <title>Hello World</title>
+    </Head>
+    <div>Hello World!</div> 
+    </>
+  )
+}
+
+export default Home
